0fa36912ebe432f76c946f612fb3b89e0732a14f
Paul Fox <studio@gibbitz.com>
2024-08-05 10:42:21 -0400

    feat : Added difficulty translations =====================================
    ------------------------------------
    [ NA ] gibbitz

--- src/module/constants/difficulty.js
+++ src/module/constants/difficulty.js
@@ -47,6 +47,38 @@ export const TO_HIT_MODIFIERS ={
   [`${BASE_SELECT_TO_HIT_I18N}.fullAutoClose`]: 1,
   [`${BASE_SELECT_TO_HIT_I18N}.fullAutoOther`]: -1
 }
+const BASE_SELECT_DIFF_I18N = 'cp2020.selects.modifiers.difficulty'
 export const SKILL_MODIFIERS = {
-
+  [`${BASE_SELECT_DIFF_I18N}.complexRepair`]: 2,
+  [`${BASE_SELECT_DIFF_I18N}.veryComplex`]: 4,
+  [`${BASE_SELECT_DIFF_I18N}.neverDone`]: 6,
+  [`${BASE_SELECT_DIFF_I18N}.wrongParts`]: 2,
+  [`${BASE_SELECT_DIFF_I18N}.wrongTools`]: 3,
+  [`${BASE_SELECT_DIFF_I18N}.unfamiliar`]: 4,
+  [`${BASE_SELECT_DIFF_I18N}.stress`]: 3,
+  [`${BASE_SELECT_DIFF_I18N}.attack`]: 3,
+  [`${BASE_SELECT_DIFF_I18N}.siege`]: 4,
+  [`${BASE_SELECT_DIFF_I18N}.wounded1`]: 2,
+  [`${BASE_SELECT_DIFF_I18N}.wounded2`]: 4,
+  [`${BASE_SELECT_DIFF_I18N}.wounded3`]: 6,
+  [`${BASE_SELECT_DIFF_I18N}.drunk`]: 4,
+  [`${BASE_SELECT_DIFF_I18N}.hostile`]: 4,
+  [`${BASE_SELECT_DIFF_I18N}.veryHostile`]: 6,
+  [`${BASE_SELECT_DIFF_I18N}.noInstructions`]: 2,
+  [`${BASE_SELECT_DIFF_I18N}.kibbitzing`]: 3,
+  [`${BASE_SELECT_DIFF_I18N}.haveNotDone`]: 1,
+  [`${BASE_SELECT_DIFF_I18N}.difficultAcrobatics`]: 3,
+  [`${BASE_SELECT_DIFF_I18N}.veryDifficultAcrobatics`]: 4,
+  [`${BASE_SELECT_DIFF_I18N}.impossibleAcrobatics`]: 5,
+  [`${BASE_SELECT_DIFF_I18N}.hiddenInfo`]: 3,
+  [`${BASE_SELECT_DIFF_I18N}.wellHiddenItem`]: 3,
+  [`${BASE_SELECT_DIFF_I18N}.complexProgram`]: 3,
+  [`${BASE_SELECT_DIFF_I18N}.veryComplexProgram`]: 5,
+  [`${BASE_SELECT_DIFF_I18N}.complexLock`]: 3,
+  [`${BASE_SELECT_DIFF_I18N}.veryComplexLock`]: 5,
+  [`${BASE_SELECT_DIFF_I18N}.targetOnGuard`]: 3,
+  [`${BASE_SELECT_DIFF_I18N}.hiLight`]: 3,
+  [`${BASE_SELECT_DIFF_I18N}.lowLight`]: 3,
+  [`${BASE_SELECT_DIFF_I18N}.pitchDark`]: 4,
+  [`${BASE_SELECT_DIFF_I18N}.observed`]: 4
 }
